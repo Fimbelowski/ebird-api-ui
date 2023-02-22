ddbff23f64cbaa26cce3c3221c734e8ea9d58d90
Christopher <ChrFimbel@gmail.com>
2023-02-22 11:49:33 -0700

    Create BackInput.tsx

---
+++ src/components/BackInput.tsx
@@ -0,0 +1,15 @@
+import NumberInput from './NumberInput';
+import type NumberInputProps from '../types/NumberInputProps';
+
+type Props = Omit<NumberInputProps, 'label' | 'max' | 'min' | 'placeholder'>;
+
+export default function BackInput(props: Props) {
+  return (
+    <NumberInput
+      {...props}
+      label="Back"
+      max={30}
+      min={1}
+    />
+  );
+}
