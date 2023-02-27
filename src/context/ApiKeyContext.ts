534146b2804c90e897fe6fcc610ec45e40a867ad
Christopher <ChrFimbel@gmail.com>
2023-02-27 15:15:19 -0700

    Allow pages to set necessity of API Key.

--- src/context/ApiKeyContext.ts
+++ src/context/ApiKeyContext.ts
@@ -2,12 +2,16 @@ import { createContext } from 'react';
 
 interface ApiKeyContextInterface {
   apiKey: string;
+  required: boolean;
   setApiKey: (newValue: string) => void;
+  setRequired: (newValue: boolean) => void;
 }
 
 const ApiKeyContext = createContext<ApiKeyContextInterface>({
   apiKey: '',
+  required: false,
   setApiKey: () => {},
+  setRequired: () => {},
 });
 
 export default ApiKeyContext;
