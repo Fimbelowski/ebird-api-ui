381c216089bd5223af625e70562a4979096d337f
Christopher <ChrFimbel@gmail.com>
2023-02-27 16:57:02 -0700

    Remove ApiKeyInput from Header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,9 @@
-import ApiKeyInput from './ApiKeyInput';
 import Nav from './Nav';
 
 export default function Header() {
   return (
     <header className="header">
       <h1>eBird API UI</h1>
-      <ApiKeyInput />
       <Nav />
     </header>
   );
