f81b0c1e54a4c30394e69a7ce2e95e09e2228bb3
Christopher <ChrFimbel@gmail.com>
2023-02-27 15:19:47 -0700

    Update context provider.

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -2,13 +2,22 @@ import { Outlet } from 'react-router-dom';
 import { useState } from 'react';
 
 import ApiKeyContext from '../context/ApiKeyContext';
+import type ApiKeyContextInterface from '../types/ApiKeyContext';
 import Header from '../components/Header';
 
 export default function Root() {
   const [apiKey, setApiKey] = useState('');
+  const [apiKeyRequired, setApiKeyRequired] = useState(false);
+
+  const apiKeyContext: ApiKeyContextInterface = {
+    apiKey,
+    required: apiKeyRequired,
+    setApiKey,
+    setRequired: setApiKeyRequired,
+  };
 
   return (
-    <ApiKeyContext.Provider value={{ apiKey, setApiKey }}>
+    <ApiKeyContext.Provider value={apiKeyContext}>
       <Header></Header>
       <main>
         <Outlet />
