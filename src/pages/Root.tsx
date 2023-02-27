4745a1637b4960b15c1188e06bcc9023e3d38434
Christopher <ChrFimbel@gmail.com>
2023-02-27 14:50:52 -0700

    Add context provider.

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,14 +1,18 @@
 import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
 
+import ApiKeyContext from '../context/ApiKeyContext';
 import Header from '../components/Header';
 
 export default function Root() {
+  const [apiKey, setApiKey] = useState('');
+
   return (
-    <>
+    <ApiKeyContext.Provider value={{ apiKey, setApiKey }}>
       <Header></Header>
       <main>
         <Outlet />
       </main>
-    </>
+    </ApiKeyContext.Provider>
   );
 }
