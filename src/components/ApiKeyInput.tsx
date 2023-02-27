85bc2acf0f3f20c8078f501c86ce0eff19a6467a
Christopher <ChrFimbel@gmail.com>
2023-02-27 15:01:15 -0700

    Create ApiKeyInput component.

---
+++ src/components/ApiKeyInput.tsx
@@ -0,0 +1,47 @@
+import { type ChangeEvent, useContext, useState } from 'react';
+
+import ApiKeyContext from '../context/ApiKeyContext';
+import BaseInput from './BaseInput';
+import Button from './Button';
+import getValueFromChangeEvent from '../utilities/getValueFromChangeEvent';
+
+export default function ApiKeyInput() {
+  const { apiKey, setApiKey } = useContext(ApiKeyContext);
+
+  const [showApiKey, setShowApiKey] = useState(false);
+
+  function inputType() {
+    return showApiKey ? 'text' : 'password';
+  }
+
+  function onChange(event: ChangeEvent<HTMLInputElement>) {
+    const value = getValueFromChangeEvent(event);
+    setApiKey(value);
+  }
+
+  function onToggleShowApiKey() {
+    setShowApiKey(!showApiKey);
+  }
+
+  function buttonLabel() {
+    return showApiKey ? 'Hide' : 'Show';
+  }
+
+  return (
+    <div className="api-key-input">
+      <BaseInput
+        id="api-key"
+        label="Api Key"
+        onChange={onChange}
+        type={inputType()}
+        value={apiKey}
+      />
+      <Button
+        onClick={onToggleShowApiKey}
+        type="button"
+      >
+        {buttonLabel()}
+      </Button>
+    </div>
+  );
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
+import ApiKeyInput from './ApiKeyInput';
 import Nav from './Nav';
 
 export default function Header() {
   return (
     <header className="header">
       <h1>eBird API UI</h1>
+      <ApiKeyInput />
       <Nav />
     </header>
   );
