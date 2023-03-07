f2b4990f27e8d20846a4e87fbb1125856222e98c
Christopher <ChrFimbel@gmail.com>
2023-03-06 18:23:17 -0700

    Flesh out functionality for RegionStatsOnDate.

--- src/pages/product/RegionalStatsOnDate.tsx
+++ src/pages/product/RegionalStatsOnDate.tsx
@@ -2,7 +2,13 @@ import { useState } from 'react';
 
 import BasePage from '../../components/BasePage';
 import DateInput from '../../components/DateInput';
+import dateStringToObject from '../../utilities/dateStringToObject';
+import Details from '../../components/Details';
 import EbirdRegionCodeInput from '../../components/EbirdRegionCodeInput';
+import type EbirdRegionStats from '../../types/EbirdRegionStats';
+import Table from '../../components/Table';
+import type TableCell from '../../types/TableCell';
+import type TableHeader from '../../types/TableHeader';
 import useApiKey from '../../hooks/useApiKey';
 import useEbirdApi from '../../hooks/useEbirdApi';
 import useRequestState from '../../hooks/useRequestState';
@@ -17,9 +23,61 @@ export default function RegionalStatsOnDate() {
     setLoading,
     setRawResponse,
   } = useRequestState();
+  const { getRegionStatsOnDate } = useEbirdApi();
 
   const [date, setDate] = useState('');
   const [regionCode, setRegionCode] = useState('');
+  const [stats, setStats] = useState<EbirdRegionStats>();
+
+  const tableCells: Array<TableCell<EbirdRegionStats>> = [
+    {
+      align: 'right',
+      callback: ({ numChecklists }) => numChecklists,
+    },
+    {
+      align: 'right',
+      callback: ({ numContributors }) => numContributors,
+    },
+    {
+      align: 'right',
+      callback: ({ numSpecies }) => numSpecies,
+    },
+  ];
+
+  const tableHeaders: TableHeader[] = [
+    {
+      align: 'right',
+      label: 'Checklists',
+    },
+    {
+      align: 'right',
+      label: 'Contributors',
+    },
+    {
+      align: 'right',
+      label: 'Species',
+    },
+  ];
+
+  function onSubmit() {
+    setLoading(true);
+
+    const { year, month, day } = dateStringToObject(date);
+
+    getRegionStatsOnDate(apiKey, regionCode, year, month, day)
+      .then(async (response) => await response.text())
+      .then((data) => {
+        setRawResponse(data);
+        setStats(JSON.parse(data));
+        setHasQueried(true);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
 
   const formContent = (
     <>
@@ -37,13 +95,29 @@ export default function RegionalStatsOnDate() {
     </>
   );
 
+  const resultsContent =
+    stats === undefined ? null : (
+      <Details
+        open
+        summary="Results Table"
+      >
+        <Table<EbirdRegionStats>
+          cells={tableCells}
+          headers={tableHeaders}
+          items={[stats]}
+        />
+      </Details>
+    );
+
   return (
     <BasePage
       formContent={formContent}
       hasQueried={hasQueried}
       loading={loading}
+      onFormSubmit={onSubmit}
       rawResponse={rawResponse}
       requiresApiKey
+      resultsContent={resultsContent}
       title="Regional Statistics on a Date"
     />
   );
