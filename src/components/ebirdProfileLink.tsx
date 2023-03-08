111f369133cabf3ce337038a36b60551678d4df3
Christopher <ChrFimbel@gmail.com>
2023-03-08 10:25:27 -0700

    Create ebirdProfileLink.tsx

---
+++ src/components/ebirdProfileLink.tsx
@@ -0,0 +1,22 @@
+import type EbirdContributor from '../types/EbirdContributor';
+
+type Props = Pick<EbirdContributor, 'userDisplayName'> & {
+  profileHandle: string;
+};
+
+export default function ebirdProfileLink({
+  profileHandle,
+  userDisplayName,
+}: Props) {
+  const baseUrl = 'https://ebird.org/profile/';
+
+  return (
+    <a
+      href={`${baseUrl}${profileHandle}`}
+      rel="noreferrer"
+      target="_blank"
+    >
+      {userDisplayName}
+    </a>
+  );
+}
