d4ad8ea56657eeb720958eeb849c6dc12290b2b5
Christopher <ChrFimbel@gmail.com>
2023-03-08 14:15:39 -0700

    Create page form content.

--- src/pages/product/ChecklistFeedOnDate.tsx
+++ src/pages/product/ChecklistFeedOnDate.tsx
@@ -1,4 +1,11 @@
+import { useState } from 'react';
+
 import BasePage from '../../components/BasePage';
+import type EbirdChecklistSortKey from '../../types/EbirdChecklistSortKey';
+import EbirdRegionCodeInput from '../../components/EbirdRegionCodeInput';
+import NumberInput from '../../components/NumberInput';
+import Select from '../../components/Select';
+import type SelectOption from '../../types/SelectOption';
 import useApiKey from '../../hooks/useApiKey';
 import useDate from '../../hooks/useDate';
 import useEbirdApi from '../../hooks/useEbirdApi';
@@ -6,7 +13,7 @@ import useRequestState from '../../hooks/useRequestState';
 
 export default function ChecklistFeedOnDate() {
   const { apiKey } = useApiKey();
-  const { DateInput, day, month, onChange, year } = useDate();
+  const { DateInput, day, month, onChange: onDateChange, year } = useDate();
   const ebirdApi = useEbirdApi();
   const {
     hasQueried,
@@ -17,11 +24,56 @@ export default function ChecklistFeedOnDate() {
     setRawResponse,
   } = useRequestState();
 
+  const [maxResults, setMaxResults] = useState('10');
+  const [regionCode, setRegionCode] = useState('');
+  const [sortKey, setSortKey] = useState<EbirdChecklistSortKey>('obs_dt');
+
+  const sortKeySelectOptions: Array<SelectOption<EbirdChecklistSortKey>> = [
+    {
+      label: 'Observation Date',
+      value: 'obs_dt',
+    },
+    {
+      label: 'Creation Date',
+      value: 'creation_dt',
+    },
+  ];
+
+  const formContent = (
+    <>
+      <EbirdRegionCodeInput
+        onChange={setRegionCode}
+        required
+        value={regionCode}
+      />
+      <DateInput
+        id="date"
+        onChange={onDateChange}
+        required
+      />
+      <Select<EbirdChecklistSortKey>
+        id="sort-key"
+        label="Sort By"
+        onChange={setSortKey}
+        options={sortKeySelectOptions}
+        value={sortKey}
+      />
+      <NumberInput
+        id="max-results"
+        label="Max Results"
+        onChange={setMaxResults}
+        value={maxResults}
+      />
+    </>
+  );
+
   return (
     <BasePage
+      formContent={formContent}
       hasQueried={hasQueried}
       loading={loading}
       rawResponse={rawResponse}
+      requiresApiKey
       title="Checklist Feed on a Date"
     />
   );
