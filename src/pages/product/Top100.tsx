4955a53c1ab5dcffb5830f4721e28a6a721024ca
Christopher <ChrFimbel@gmail.com>
2023-03-07 17:32:32 -0700

    Create form content.

--- src/pages/product/Top100.tsx
+++ src/pages/product/Top100.tsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
+
 import BasePage from '../../components/BasePage';
+import type EbirdRankedBy from '../../types/EbirdRankedBy';
+import EbirdRegionCodeInput from '../../components/EbirdRegionCodeInput';
+import NumberInput from '../../components/NumberInput';
+import Select from '../../components/Select';
+import type SelectOption from '../../types/SelectOption';
 import useDate from '../../hooks/useDate';
 import useEbirdApi from '../../hooks/useEbirdApi';
 import useRequestState from '../../hooks/useRequestState';
 
 export default function Top100() {
-  const { day, month, onChange: onDateChange, year } = useDate();
+  const { DateInput, day, month, onChange: onDateChange, year } = useDate();
   const {
     hasQueried,
     loading,
@@ -15,8 +22,55 @@ export default function Top100() {
   } = useRequestState();
   const { getTop100 } = useEbirdApi();
 
+  const [maxResults, setMaxResults] = useState('');
+  const [rankedBy, setRankedBy] = useState<EbirdRankedBy>('spp');
+  const [regionCode, setRegionCode] = useState('');
+
+  const rankedByOptions: Array<SelectOption<EbirdRankedBy>> = [
+    {
+      label: 'Checklists',
+      value: 'cl',
+    },
+    {
+      label: 'Species',
+      value: 'spp',
+    },
+  ];
+
+  const formContent = (
+    <>
+      <EbirdRegionCodeInput
+        onChange={setRegionCode}
+        required
+        value={regionCode}
+      />
+      <DateInput
+        id="date"
+        onChange={onDateChange}
+        required
+      />
+      <Select<EbirdRankedBy>
+        id="ranked-by"
+        label="Ranked By"
+        onChange={setRankedBy}
+        options={rankedByOptions}
+        value={rankedBy}
+      />
+      <NumberInput
+        id="max-results"
+        label="Max Results"
+        max={100}
+        min={1}
+        onChange={setMaxResults}
+        placeholder="100"
+        value={maxResults}
+      />
+    </>
+  );
+
   return (
     <BasePage
+      formContent={formContent}
       hasQueried={hasQueried}
       loading={loading}
       rawResponse={rawResponse}
