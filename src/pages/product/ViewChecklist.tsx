10b19febd752b45cc857c393b8966d94f95df3f6
Christopher <ChrFimbel@gmail.com>
2023-03-09 13:20:49 -0700

    Fix case.

--- src/pages/product/ViewChecklist.tsx
+++ src/pages/product/ViewChecklist.tsx
@@ -2,7 +2,7 @@ import BasePage from '../../components/BasePage';
 import useEbirdApi from '../../hooks/useEbirdApi';
 import useRequestState from '../../hooks/useRequestState';
 
-export default function viewChecklist() {
+export default function ViewChecklist() {
   const ebirdApi = useEbirdApi();
   const {
     hasQueried,
