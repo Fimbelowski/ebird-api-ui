eb4fb9e1f85751b608a5095be1e270ae27518356
Christopher <ChrFimbel@gmail.com>
2023-03-22 09:40:25 -0600

    Add LoadingContext.

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -3,16 +3,20 @@ import { useState } from 'react';
 
 import ApiKeyContext from '../context/ApiKeyContext';
 import Header from '../components/Header';
+import LoadingContext from '../context/LoadingContext';
 
 export default function Root() {
   const [apiKey, setApiKey] = useState('');
+  const [loading, setLoading] = useState(false);
 
   return (
     <>
       <Header></Header>
       <main>
         <ApiKeyContext.Provider value={{ apiKey, setApiKey }}>
-          <Outlet />
+          <LoadingContext.Provider value={{ loading, setLoading }}>
+            <Outlet />
+          </LoadingContext.Provider>
         </ApiKeyContext.Provider>
       </main>
     </>
