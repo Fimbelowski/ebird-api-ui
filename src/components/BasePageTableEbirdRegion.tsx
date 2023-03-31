2589a892040abe02b20a1a7ff92aada377fa888c
Christopher <Fimbelowski@users.noreply.github.com>
2023-03-31 17:30:39 -0600

    Base page table ebird region (#31)
    * Create BasePageTableEbirdRegion.tsx
    
    * Migrate some pages to BasePageTableEbirdRegion.

---
+++ src/components/BasePageTableEbirdRegion.tsx
@@ -0,0 +1,40 @@
+import {
+  BasePageTable,
+  type BasePageTableProps,
+  type Tables,
+} from './BasePageTable';
+import type EbirdRegion from '../types/EbirdRegion';
+
+type Props = Omit<BasePageTableProps<EbirdRegion>, 'tables'>;
+
+export default function BasePageTableEbirdRegion(props: Props) {
+  const tables: Tables<EbirdRegion> = [
+    {
+      cells: [
+        {
+          callback: ({ name }) => name,
+        },
+        {
+          callback: ({ code }) => code,
+        },
+      ],
+      headers: [
+        {
+          label: 'Name',
+        },
+        {
+          label: 'Code',
+        },
+      ],
+      open: true,
+      title: 'Results Table',
+    },
+  ];
+
+  return (
+    <BasePageTable
+      {...props}
+      tables={tables}
+    />
+  );
+}
