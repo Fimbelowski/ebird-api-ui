ea9d9d225930e0cad68824d54bebc3c448a510b5
Christopher <ChrFimbel@gmail.com>
2023-04-05 15:58:16 -0600

    Remove unused types.

--- src/types/NumberInputProps.ts
+++
@@ -1,9 +0,0 @@
-import { type BaseInputProps } from '../components/BaseInput';
-
-export default interface NumberInputProps extends BaseInputProps {
-  hideStepper?: boolean;
-  noScroll?: boolean;
-  max?: number;
-  min?: number;
-  step?: number | 'any';
-}
