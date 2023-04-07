539a41eb2279ed660b775cee44e5aa4f594f29c4
Christopher <ChrFimbel@gmail.com>
2023-04-07 11:57:35 -0600

    Update NumberInput.tsx

--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -2,14 +2,30 @@ import { BaseInput, type BaseInputProps } from './BaseInput';
 
 export type NumberInputProps = Omit<
   BaseInputProps,
-  'maxLength' | 'minLength' | 'type'
->;
+  'onChange' | 'maxLength' | 'minLength' | 'type' | 'value'
+> & { onChange: (value: number) => void; value?: number };
 
-export function NumberInput(props: NumberInputProps) {
+export function NumberInput({
+  onChange: onChangeProp,
+  value,
+  ...rest
+}: NumberInputProps) {
+  function onChange(value: string) {
+    const valueAsNumber = parseFloat(value);
+
+    onChangeProp(valueAsNumber);
+  }
+
+  function valueAsString() {
+    return value?.toString() ?? '';
+  }
+
   return (
     <BaseInput
-      {...props}
+      {...rest}
+      onChange={onChange}
       type="number"
+      value={valueAsString()}
     />
   );
 }
