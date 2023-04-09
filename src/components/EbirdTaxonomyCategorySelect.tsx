e17f3d9c2589d7c38dd845acac68dd6f2f99b61d
Christopher <ChrFimbel@gmail.com>
2023-04-09 14:10:06 -0600

    Add CategorySelect component.

---
+++ src/components/EbirdTaxonomyCategorySelect.tsx
@@ -0,0 +1,57 @@
+import type EbirdTaxonomyCategory from '../types/EbirdTaxonomyCategory';
+import { Select, type SelectProps, type SelectOptionArray } from './Select';
+
+type Props = Omit<
+  SelectProps<EbirdTaxonomyCategory>,
+  'id' | 'label' | 'options'
+>;
+
+export default function EbirdTaxonomyCategorySelect(props: Props) {
+  const options: SelectOptionArray<EbirdTaxonomyCategory> = [
+    {
+      label: 'All',
+      value: '',
+    },
+    {
+      label: 'Domestic',
+      value: 'domestic',
+    },
+    {
+      label: 'Form',
+      value: 'form',
+    },
+    {
+      label: 'Hybrid',
+      value: 'hybrid',
+    },
+    {
+      label: 'Intergrade',
+      value: 'intergrade',
+    },
+    {
+      label: 'Identifiable Sub-specific Group',
+      value: 'issf',
+    },
+    {
+      label: 'Slash',
+      value: 'slash',
+    },
+    {
+      label: 'Species',
+      value: 'species',
+    },
+    {
+      label: 'Spuh',
+      value: 'spuh',
+    },
+  ];
+
+  return (
+    <Select<EbirdTaxonomyCategory>
+      {...props}
+      id="category"
+      label="Category"
+      options={options}
+    />
+  );
+}
---
+++ src/types/EbirdTaxonomyCategory.ts
@@ -0,0 +1,13 @@
+// https://science.ebird.org/en/use-ebird-data/the-ebird-taxonomy
+type EbirdTaxonomyCategory =
+  | ''
+  | 'domestic'
+  | 'form'
+  | 'hybrid'
+  | 'intergrade'
+  | 'issf'
+  | 'slash'
+  | 'species'
+  | 'spuh';
+
+export default EbirdTaxonomyCategory;
