e8573c0949d088011e057bb2407273d411baa0e0
Christopher <ChrFimbel@gmail.com>
2023-04-09 14:09:52 -0600

    Add LocaleSelect component.

---
+++ src/components/LocaleSelect.tsx
@@ -0,0 +1,68 @@
+import { useContext, useState } from 'react';
+
+import Button from './Button';
+import LocaleOptionsContext from '../context/LocaleOptionsContext';
+import { Select, type SelectProps } from './Select';
+import useApiKey from '../hooks/useApiKey';
+import useEbirdApi from '../hooks/useEbirdApi';
+
+type Props = Omit<SelectProps<string>, 'id' | 'label' | 'options'>;
+
+export default function LocaleSelect({ disabled, ...rest }: Props) {
+  const { localeOptions, setLocaleOptions } = useContext(LocaleOptionsContext);
+
+  const { apiKey } = useApiKey();
+  const { getTaxaLocaleCodes } = useEbirdApi();
+
+  const [hasQueried, setHasQueried] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  function hasApiKey() {
+    return apiKey !== '';
+  }
+
+  function loadMoreButtonLabel() {
+    return isLoading ? 'Loading...' : 'Load More';
+  }
+
+  function onLoadMoreClick() {
+    setIsLoading(true);
+
+    getTaxaLocaleCodes()
+      .then(({ parsedResponse }) => {
+        setLocaleOptions(
+          parsedResponse.map(({ code, name }) => ({ label: name, value: code }))
+        );
+        setHasQueried(true);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
+
+  return (
+    <div className="locale-select">
+      <div className="locale-select__select">
+        <Select<string>
+          {...rest}
+          disabled={(disabled ?? false) || !hasQueried}
+          id="locale"
+          label="Locale"
+          options={localeOptions}
+        />
+      </div>
+      {hasQueried ? null : (
+        <Button
+          disabled={!hasApiKey() || isLoading}
+          onClick={onLoadMoreClick}
+          type="button"
+        >
+          {loadMoreButtonLabel()}
+        </Button>
+      )}
+    </div>
+  );
+}
