3e422373236ffd767604735210d23e98a4364e3c
Christopher <ChrFimbel@gmail.com>
2023-04-18 14:34:49 -0600

    Update RecentObservationsRegion.tsx

--- src/pages/obs/RecentObservationsRegion.tsx
+++ src/pages/obs/RecentObservationsRegion.tsx
@@ -1,13 +1,11 @@
 import { useState } from 'react';
 
-import { BasePageTable, type Tables } from '../../components/BasePageTable';
+import BasePageTableEbirdObservation from '../../components/BasePageTableEbirdObservation';
 import BackInput from '../../components/BackInput';
 import CheckboxInput from '../../components/CheckboxInput';
-import type EbirdObservation from '../../types/EbirdObservation';
 import type EbirdTaxonomyCategory from '../../types/EbirdTaxonomyCategory';
 import EbirdRegionCodeInput from '../../components/EbirdRegionCodeInput';
 import EbirdTaxonomyCategorySelect from '../../components/EbirdTaxonomyCategorySelect';
-import GoogleMapsLink from '../../components/GoogleMapsLink';
 import LocaleSelect from '../../components/LocaleSelect';
 import LocationTextarea from '../../components/LocationTextarea';
 import { NumberInput } from '../../components/NumberInput';
@@ -24,145 +22,6 @@ export default function RecentObservationsRegion() {
   const [maxResults, setMaxResults] = useState<number>();
   const [onlyObsFromHotspots, setOnlyObsFromHotspots] = useState(false);
   const [regionCode, setRegionCode] = useState('');
-
-  const tables: Tables<EbirdObservation> = [
-    {
-      cells: [
-        {
-          callback: ({ comName }) => comName,
-        },
-        {
-          callback: ({ exoticCategory }) => exoticCategory,
-        },
-        {
-          align: 'right',
-          callback: ({ howMany }) => howMany?.toString(),
-        },
-        {
-          align: 'right',
-          callback: ({ lat }) => lat.toString(),
-        },
-        {
-          align: 'right',
-          callback: ({ lng }) => lng.toString(),
-        },
-        {
-          callback: ({ locId }) => locId,
-        },
-        {
-          callback: ({ locName }) => locName,
-        },
-        {
-          callback: ({ locationPrivate }) => locationPrivate,
-        },
-        {
-          callback: ({ obsDt }) => obsDt,
-        },
-        {
-          callback: ({ obsReviewed }) => obsReviewed,
-        },
-        {
-          callback: ({ obsValid }) => obsValid,
-        },
-        {
-          callback: ({ sciName }) => sciName,
-        },
-        {
-          callback: ({ speciesCode }) => speciesCode,
-        },
-        {
-          callback: ({ subId }) => subId,
-        },
-      ],
-      headers: [
-        {
-          label: 'comName',
-        },
-        {
-          label: 'exoticCategory',
-        },
-        {
-          align: 'right',
-          label: 'howMany',
-        },
-        {
-          align: 'right',
-          label: 'lat',
-        },
-        {
-          align: 'right',
-          label: 'lng',
-        },
-        {
-          label: 'locId',
-        },
-        {
-          label: 'locName',
-        },
-        {
-          label: 'locationPrivate',
-        },
-        {
-          label: 'obsDt',
-        },
-        {
-          label: 'obsReviewed',
-        },
-        {
-          label: 'obsValid',
-        },
-        {
-          label: 'sciName',
-        },
-        {
-          label: 'speciesCode',
-        },
-        {
-          label: 'subId',
-        },
-      ],
-      title: 'Detailed Table',
-    },
-    {
-      cells: [
-        {
-          callback: ({ comName }) => comName,
-        },
-        {
-          callback: ({ howMany }) => howMany?.toLocaleString(),
-        },
-        {
-          callback: ({ lat, lng, locName }) => (
-            <GoogleMapsLink
-              latitude={lat}
-              longitude={lng}
-            >
-              {locName}
-            </GoogleMapsLink>
-          ),
-        },
-        {
-          callback: ({ obsDt }) => new Date(obsDt).toLocaleString(),
-        },
-      ],
-      headers: [
-        {
-          label: 'Species',
-        },
-        {
-          label: 'Quantity',
-        },
-        {
-          label: 'Location',
-        },
-        {
-          label: 'Date',
-        },
-      ],
-      open: true,
-      title: 'Simple Table',
-    },
-  ];
 
   async function onSubmit() {
     return await getRecentObservationsInARegion(
@@ -224,11 +83,10 @@ export default function RecentObservationsRegion() {
   );
 
   return (
-    <BasePageTable<EbirdObservation>
+    <BasePageTableEbirdObservation
       formContent={formContent}
       onSubmit={onSubmit}
       requiresApiKey
-      tables={tables}
       title="Recent Observations in a Region"
     />
   );
