c4608b8dc1df521b27efce0b22698dabeca26c4f
Christopher <ChrFimbel@gmail.com>
2023-04-19 10:49:56 -0600

    Stub out state.

--- src/pages/obs/RecentObservationsOfSpeciesInRegion.tsx
+++ src/pages/obs/RecentObservationsOfSpeciesInRegion.tsx
@@ -1,3 +1,14 @@
+import { useState } from 'react';
+
 export default function RecentObservationsOfSpeciesInRegion() {
+  const [back, setBack] = useState(14);
+  const [includeProvisionalObs, setIncludeProvisionalObs] = useState(false);
+  const [locale, setLocale] = useState('en');
+  const [maxResults, setMaxResults] = useState<number>();
+  const [obsLocations, setObsLocations] = useState<string[]>([]);
+  const [onlyObsFromHotspots, setOnlyObsFromHotspots] = useState(false);
+  const [regionCode, setRegionCode] = useState('');
+  const [speciesCode, setSpeciesCode] = useState('');
+
   return <></>;
 }
