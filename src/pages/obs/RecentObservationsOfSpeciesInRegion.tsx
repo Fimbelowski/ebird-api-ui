bfbe8f0d58228e3e926fe1890ba4cb77d98d3465
Christopher <ChrFimbel@gmail.com>
2023-04-19 11:02:41 -0600

    Add formContent.

--- src/pages/obs/RecentObservationsOfSpeciesInRegion.tsx
+++ src/pages/obs/RecentObservationsOfSpeciesInRegion.tsx
@@ -1,6 +1,14 @@
 import { useState } from 'react';
 
+import BackInput from '../../components/BackInput';
 import BasePageTableEbirdObservation from '../../components/BasePageTableEbirdObservation';
+import EbirdIncludeProvisionalInput from '../../components/EbirdIncludeProvisionalInput';
+import EbirdOnlyObsFromHotspotsInput from '../../components/EbirdOnlyObsFromHotspotsInput';
+import EbirdRegionCodeInput from '../../components/EbirdRegionCodeInput';
+import EbirdSpeciesCodeInput from '../../components/EbirdSpeciesCodeInput';
+import LocaleSelect from '../../components/LocaleSelect';
+import LocationTextarea from '../../components/LocationTextarea';
+import MaxResultsInput from '../../components/MaxResultsInput';
 import useEbirdApi from '../../hooks/useEbirdApi';
 
 export default function RecentObservationsOfSpeciesInRegion() {
@@ -28,8 +36,48 @@ export default function RecentObservationsOfSpeciesInRegion() {
     );
   }
 
+  const formContent = (
+    <>
+      <EbirdRegionCodeInput
+        onChange={setRegionCode}
+        required
+        value={regionCode}
+      />
+      <EbirdSpeciesCodeInput
+        onChange={setSpeciesCode}
+        required
+        value={speciesCode}
+      />
+      <BackInput
+        onChange={setBack}
+        value={back}
+      />
+      <EbirdOnlyObsFromHotspotsInput
+        onChange={setOnlyObsFromHotspots}
+        value={onlyObsFromHotspots}
+      />
+      <EbirdIncludeProvisionalInput
+        onChange={setIncludeProvisionalObs}
+        value={includeProvisionalObs}
+      />
+      <MaxResultsInput
+        onChange={setMaxResults}
+        value={maxResults}
+      />
+      <LocationTextarea
+        onChange={setObsLocations}
+        value={obsLocations}
+      />
+      <LocaleSelect
+        onChange={setLocale}
+        value={locale}
+      />
+    </>
+  );
+
   return (
     <BasePageTableEbirdObservation
+      formContent={formContent}
       onSubmit={onSubmit}
       title="Recent Observations of a Species in a Region"
     />
