5d55cee973808babf4bfb3a876bc341a871e0b68
Christopher <ChrFimbel@gmail.com>
2023-06-06 13:47:09 -0600

    Create useRegionInfo.ts

---
+++ src/services/ebird/hooks/endpoints/ref/region/useRegionInfo.ts
@@ -0,0 +1,25 @@
+import useEbirdApi from '../../../useEbirdApi';
+import { type QueryParam, type UrlParam } from '../../../../ebirdApiClient';
+
+type EbirdRegionNameFormat =
+  | 'detailed'
+  | 'detailednoqual'
+  | 'full'
+  | 'namequal'
+  | 'nameonly'
+  | 'revdetailed';
+
+export default async function useRegionInfo(
+  regionCode: string,
+  regionNameFormat: EbirdRegionNameFormat = 'full',
+  delimiter = ', '
+) {
+  const urlParams: UrlParam[] = [];
+
+  const queryParams: QueryParam[] = [];
+
+  return await useEbirdApi('ref/region/info/{{regionCode}}', {
+    urlParams,
+    queryParams,
+  });
+}
