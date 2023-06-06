22f4d725016f88d17d7e638a65b660a376c192be
Christopher <ChrFimbel@gmail.com>
2023-06-06 11:52:31 -0600

    Create "useEbirdApi" hook.

---
+++ src/services/ebird/hooks/useEbirdApi.ts
@@ -0,0 +1,12 @@
+import type { EbirdApiParams } from '../ebirdApiClient';
+import { makeRequest } from '../ebirdApiClient';
+import useApiKey from '../../../hooks/useApiKey';
+
+export default async function useEbirdApi(
+  endpoint: string,
+  options: Omit<EbirdApiParams, 'apiKey'>
+) {
+  const { apiKey } = useApiKey();
+
+  return await makeRequest(endpoint, { ...options, apiKey });
+}
