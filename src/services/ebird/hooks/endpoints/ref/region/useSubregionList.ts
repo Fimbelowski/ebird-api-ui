88a52cbd7093cfa7b769cd77e06344807bee4ce6
Christopher <ChrFimbel@gmail.com>
2023-06-06 14:40:57 -0600

    Update useSubregionList.ts

--- src/services/ebird/hooks/endpoints/ref/region/useSubregionList.ts
+++ src/services/ebird/hooks/endpoints/ref/region/useSubregionList.ts
@@ -1,14 +1,13 @@
+import type EbirdRecordFormat from '../../../../types/EbirdRecordFormat';
 import { type QueryParam, type UrlParam } from '../../../../ebirdApiClient';
 import useEbirdApi from '../../../useEbirdApi';
 
 export type EbirdRegionType = 'country' | 'subnational1' | 'subnational2';
 
-export type EbirdResponseFormat = 'csv' | 'json';
-
 export async function useSubregionList(
   regionType: EbirdRegionType,
   parentRegionCode: string,
-  format: EbirdResponseFormat = 'json'
+  format: EbirdRecordFormat = 'json'
 ) {
   const urlParams: UrlParam[] = [
     {
