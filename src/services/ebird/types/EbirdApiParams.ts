cfed4f8032d95bb63841198b8e509fad960373a3
Christopher <ChrFimbel@gmail.com>
2023-06-07 12:46:58 -0600

    Add types related to query params.

--- src/services/ebird/types/EbirdApiParams.ts
+++ src/services/ebird/types/EbirdApiParams.ts
@@ -2,3 +2,17 @@ export interface UrlParam {
   name: string;
   value: string | number;
 }
+
+export type QueryParamValue = boolean | number | string | string[];
+
+export interface BaseQueryParam<T extends QueryParamValue> {
+  defaultValue?: T;
+  name: string;
+  value: T | undefined;
+}
+
+export type QueryParam =
+  | BaseQueryParam<boolean>
+  | BaseQueryParam<number>
+  | BaseQueryParam<string>
+  | BaseQueryParam<string[]>;
