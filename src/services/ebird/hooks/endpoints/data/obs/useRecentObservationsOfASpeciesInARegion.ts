8ba6e0ac0b8a500ecc7f94017f55539a25c884a5
Christopher <ChrFimbel@gmail.com>
2023-06-07 10:43:05 -0600

    Create useRecentObservationsOfASpeciesInARegion.ts

---
+++ src/services/ebird/hooks/endpoints/data/obs/useRecentObservationsOfASpeciesInARegion.ts
@@ -0,0 +1,60 @@
+import useEbirdApi from '../../../useEbirdApi';
+import { type UrlParam, type QueryParam } from '../../../../ebirdApiClient';
+
+export default async function useRecentObservationsOfASpeciesInARegion(
+  regionCode: string,
+  speciesCode: string,
+  back = 14,
+  hotspot = false,
+  includeProvisional = false,
+  maxResults?: number,
+  locations?: string[],
+  speciesCommonNameLocale = 'en'
+) {
+  const urlParams: UrlParam[] = [
+    {
+      name: 'regionCode',
+      value: regionCode,
+    },
+    {
+      name: 'speciesCode',
+      value: speciesCode,
+    },
+  ];
+
+  const queryParams: QueryParam[] = [
+    {
+      defaultValue: 14,
+      name: 'back',
+      value: back,
+    },
+    {
+      defaultValue: false,
+      name: 'hotspot',
+      value: hotspot,
+    },
+    {
+      defaultValue: false,
+      name: 'includeProvisional',
+      value: includeProvisional,
+    },
+    {
+      name: 'maxResults',
+      value: maxResults,
+    },
+    {
+      name: 'r',
+      value: locations,
+    },
+    {
+      defaultValue: 'en',
+      name: 'sppLocale',
+      value: speciesCommonNameLocale,
+    },
+  ];
+
+  return await useEbirdApi('data/obs/{{regionCode}}/recent/{{speciesCode}}', {
+    urlParams,
+    queryParams,
+  });
+}
