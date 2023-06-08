ea1e359ae526ef684df65e2c114ff9b3d0663148
Christopher <ChrFimbel@gmail.com>
2023-06-08 11:55:55 -0600

    Update useNearbyHotspots.ts

--- src/services/ebird/hooks/endpoints/ref/hotspot/useNearbyHotspots.ts
+++ src/services/ebird/hooks/endpoints/ref/hotspot/useNearbyHotspots.ts
@@ -5,46 +5,45 @@ import type {
   BaseQueryParam,
 } from '../../../../types/EbirdApiParams';
 
-export default async function useNearbyHotspots(
-  latitude: number,
-  longitude: number,
-  back?: number,
-  distance = 25,
-  format: EbirdRecordFormat = 'csv'
-) {
-  /* 
-    For query parameters that use a union type of literals, the union type must be
-    specified as a generic argument, otherwise defaultValue and value are allowed
-    to be anything as long as their types match and they extend QueryParamValue.
-  */
-  const formatQueryParam: BaseQueryParam<EbirdRecordFormat> = {
-    defaultValue: 'csv',
-    name: 'fmt',
-    value: format,
+export default function useNearbyHotspots() {
+  const curriedMakeRequest = useEbirdApi();
+
+  return async function getNearbyHotspots(
+    latitude: number,
+    longitude: number,
+    back?: number,
+    distance = 25,
+    format: EbirdRecordFormat = 'csv'
+  ) {
+    const formatQueryParam: BaseQueryParam<EbirdRecordFormat> = {
+      defaultValue: 'csv',
+      name: 'fmt',
+      value: format,
+    };
+
+    const queryParams: QueryParam[] = [
+      {
+        name: 'lat',
+        value: latitude,
+      },
+      {
+        name: 'lng',
+        value: longitude,
+      },
+      {
+        name: 'back',
+        value: back,
+      },
+      {
+        defaultValue: 25,
+        name: 'dist',
+        value: distance,
+      },
+      formatQueryParam,
+    ];
+
+    return await curriedMakeRequest('ref/hotspot/geo?lat={{lat}}&lng={{lng}}', {
+      queryParams,
+    });
   };
-
-  const queryParams: QueryParam[] = [
-    {
-      name: 'lat',
-      value: latitude,
-    },
-    {
-      name: 'lng',
-      value: longitude,
-    },
-    {
-      name: 'back',
-      value: back,
-    },
-    {
-      defaultValue: 25,
-      name: 'dist',
-      value: distance,
-    },
-    formatQueryParam,
-  ];
-
-  return await useEbirdApi('ref/hotspot/geo?lat={{lat}}&lng={{lng}}', {
-    queryParams,
-  });
 }
