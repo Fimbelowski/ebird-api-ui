0f7e93d1a9373a2d021619e9a789b89c5bc9542c
Christopher <ChrFimbel@gmail.com>
2023-06-08 11:48:58 -0600

    Update useTaxonomicForms.ts

--- src/services/ebird/hooks/endpoints/ref/taxonomy/useTaxonomicForms.ts
+++ src/services/ebird/hooks/endpoints/ref/taxonomy/useTaxonomicForms.ts
@@ -1,13 +1,19 @@
 import useEbirdApi from '../../../useEbirdApi';
 import { type UrlParam } from '../../../../types/EbirdApiParams';
 
-export default async function useTaxonomicForms(speciesCode: string) {
-  const urlParams: UrlParam[] = [
-    {
-      name: 'speciesCode',
-      value: speciesCode,
-    },
-  ];
+export default function useTaxonomicForms(speciesCode: string) {
+  const curriedMakeRequest = useEbirdApi();
 
-  return await useEbirdApi('ref/taxon/forms/{{speciesCode}}', { urlParams });
+  return async function getTaxonomicForms(speciesCode: string) {
+    const urlParams: UrlParam[] = [
+      {
+        name: 'speciesCode',
+        value: speciesCode,
+      },
+    ];
+
+    return await curriedMakeRequest('ref/taxon/forms/{{speciesCode}}', {
+      urlParams,
+    });
+  };
 }
