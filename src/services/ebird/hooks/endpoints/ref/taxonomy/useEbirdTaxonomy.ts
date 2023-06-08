08a507b2edb6d7a34265230dd339b8355628615f
Christopher <ChrFimbel@gmail.com>
2023-06-08 11:51:53 -0600

    Update useEbirdTaxonomy.ts

--- src/services/ebird/hooks/endpoints/ref/taxonomy/useEbirdTaxonomy.ts
+++ src/services/ebird/hooks/endpoints/ref/taxonomy/useEbirdTaxonomy.ts
@@ -6,41 +6,45 @@ import type {
   BaseQueryParam,
 } from '../../../../types/EbirdApiParams';
 
-export async function useEbirdTaxonomy(
-  category: EbirdTaxonomyCategory,
-  format: EbirdRecordFormat = 'csv',
-  locale = 'en',
-  species: string,
-  version: number
-) {
-  const categoryQueryParam: BaseQueryParam<EbirdTaxonomyCategory> = {
-    name: 'cat',
-    value: category,
+export function useEbirdTaxonomy() {
+  const curriedMakeRequest = useEbirdApi();
+
+  return async function getEbirdTaxonomy(
+    category: EbirdTaxonomyCategory,
+    format: EbirdRecordFormat = 'csv',
+    locale = 'en',
+    species: string,
+    version: number
+  ) {
+    const categoryQueryParam: BaseQueryParam<EbirdTaxonomyCategory> = {
+      name: 'cat',
+      value: category,
+    };
+
+    const formatQueryParam: BaseQueryParam<EbirdRecordFormat> = {
+      defaultValue: 'csv',
+      name: 'fmt',
+      value: format,
+    };
+
+    const queryParams: QueryParam[] = [
+      categoryQueryParam,
+      formatQueryParam,
+      {
+        defaultValue: 'en',
+        name: 'locale',
+        value: locale,
+      },
+      {
+        name: 'species',
+        value: species,
+      },
+      {
+        name: 'version',
+        value: version,
+      },
+    ];
+
+    return await curriedMakeRequest('ref/taxonomy/ebird', { queryParams });
   };
-
-  const formatQueryParam: BaseQueryParam<EbirdRecordFormat> = {
-    defaultValue: 'csv',
-    name: 'fmt',
-    value: format,
-  };
-
-  const queryParams: QueryParam[] = [
-    categoryQueryParam,
-    formatQueryParam,
-    {
-      defaultValue: 'en',
-      name: 'locale',
-      value: locale,
-    },
-    {
-      name: 'species',
-      value: species,
-    },
-    {
-      name: 'version',
-      value: version,
-    },
-  ];
-
-  return await useEbirdApi('ref/taxonomy/ebird', { queryParams });
 }
