3b68d07cc32f7aa29a6ab44f0ec68e50027c2c33
Christopher <ChrFimbel@gmail.com>
2023-06-08 12:14:34 -0600

    Update useNearestObservationsOfASpecies.ts

--- src/services/ebird/hooks/endpoints/data/obs/useNearestObservationsOfASpecies.ts
+++ src/services/ebird/hooks/endpoints/data/obs/useNearestObservationsOfASpecies.ts
@@ -1,65 +1,69 @@
 import useEbirdApi from '../../../useEbirdApi';
 import type { UrlParam, QueryParam } from '../../../../types/EbirdApiParams';
 
-export default async function useNearestObservationsOfASpecies(
-  speciesCode: string,
-  latitude: number,
-  longitude: number,
-  back = 14,
-  hotspot = false,
-  includeProvisional = false,
-  maxResults?: number,
-  speciesCommonNameLocale = 'en',
-  distance?: number
-) {
-  const urlParams: UrlParam[] = [
-    {
-      name: 'speciesCode',
-      value: speciesCode,
-    },
-  ];
+export default function useNearestObservationsOfASpecies() {
+  const curriedMakeRequest = useEbirdApi();
 
-  const queryParams: QueryParam[] = [
-    {
-      name: 'lat',
-      value: latitude,
-    },
-    {
-      name: 'lng',
-      value: longitude,
-    },
-    {
-      defaultValue: 14,
-      name: 'back',
-      value: back,
-    },
-    {
-      defaultValue: false,
-      name: 'hotspot',
-      value: hotspot,
-    },
-    {
-      defaultValue: false,
-      name: 'includeProvisional',
-      value: includeProvisional,
-    },
-    {
-      name: 'maxResults',
-      value: maxResults,
-    },
-    {
-      defaultValue: 'en',
-      name: 'sppLocale',
-      value: speciesCommonNameLocale,
-    },
-    {
-      name: 'dist',
-      value: distance,
-    },
-  ];
+  return async function getNearestObservationOfASpecies(
+    speciesCode: string,
+    latitude: number,
+    longitude: number,
+    back = 14,
+    hotspot = false,
+    includeProvisional = false,
+    maxResults?: number,
+    speciesCommonNameLocale = 'en',
+    distance?: number
+  ) {
+    const urlParams: UrlParam[] = [
+      {
+        name: 'speciesCode',
+        value: speciesCode,
+      },
+    ];
 
-  return await useEbirdApi(
-    'data/nearest/geo/recent/{{speciesCode}}?lat={{lat}}&lng={{lng}}',
-    { urlParams, queryParams }
-  );
+    const queryParams: QueryParam[] = [
+      {
+        name: 'lat',
+        value: latitude,
+      },
+      {
+        name: 'lng',
+        value: longitude,
+      },
+      {
+        defaultValue: 14,
+        name: 'back',
+        value: back,
+      },
+      {
+        defaultValue: false,
+        name: 'hotspot',
+        value: hotspot,
+      },
+      {
+        defaultValue: false,
+        name: 'includeProvisional',
+        value: includeProvisional,
+      },
+      {
+        name: 'maxResults',
+        value: maxResults,
+      },
+      {
+        defaultValue: 'en',
+        name: 'sppLocale',
+        value: speciesCommonNameLocale,
+      },
+      {
+        name: 'dist',
+        value: distance,
+      },
+    ];
+
+    return await curriedMakeRequest(
+      'data/nearest/geo/recent/{{speciesCode}}?lat={{lat}}&lng={{lng}}',
+      { urlParams, queryParams }
+    );
+  };
 }
