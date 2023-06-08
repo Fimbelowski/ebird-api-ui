6fe06fdd5943232fd64b9e342624d37f5b0a83b0
Christopher <ChrFimbel@gmail.com>
2023-06-08 11:58:30 -0600

    Update useHotspotInfo.ts

--- src/services/ebird/hooks/endpoints/ref/hotspot/useHotspotInfo.ts
+++ src/services/ebird/hooks/endpoints/ref/hotspot/useHotspotInfo.ts
@@ -1,13 +1,19 @@
 import useEbirdApi from '../../../useEbirdApi';
 import { type UrlParam } from '../../../../types/EbirdApiParams';
 
-export default async function useHotspotInfo(locationId: string) {
-  const urlParams: UrlParam[] = [
-    {
-      name: 'locId',
-      value: locationId,
-    },
-  ];
+export default function useHotspotInfo() {
+  const curriedMakeRequest = useEbirdApi();
 
-  return await useEbirdApi('ref/hotspot/info/{{locId}}', { urlParams });
+  return async function getHotspotInfo(locationId: string) {
+    const urlParams: UrlParam[] = [
+      {
+        name: 'locId',
+        value: locationId,
+      },
+    ];
+
+    return await curriedMakeRequest('ref/hotspot/info/{{locId}}', {
+      urlParams,
+    });
+  };
 }
