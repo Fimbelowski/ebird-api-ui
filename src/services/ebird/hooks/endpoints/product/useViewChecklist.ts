0bb03e224cb8b2c54958594168fde8736dd62a23
Christopher <ChrFimbel@gmail.com>
2023-06-08 12:00:05 -0600

    Update useViewChecklist.ts

--- src/services/ebird/hooks/endpoints/product/useViewChecklist.ts
+++ src/services/ebird/hooks/endpoints/product/useViewChecklist.ts
@@ -1,13 +1,19 @@
 import useEbirdApi from '../../useEbirdApi';
 import { type UrlParam } from '../../../types/EbirdApiParams';
 
-export default async function useViewChecklist(subId: string) {
-  const urlParams: UrlParam[] = [
-    {
-      name: 'subId',
-      value: subId,
-    },
-  ];
+export default function useViewChecklist() {
+  const curriedMakeRequest = useEbirdApi();
 
-  return await useEbirdApi('product/checklist/view/{{subId}}', { urlParams });
+  return async function getChecklist(subId: string) {
+    const urlParams: UrlParam[] = [
+      {
+        name: 'subId',
+        value: subId,
+      },
+    ];
+
+    return await curriedMakeRequest('product/checklist/view/{{subId}}', {
+      urlParams,
+    });
+  };
 }
