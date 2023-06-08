8105112862ebc565f89bb410aac41fcfb49e8096
Christopher <ChrFimbel@gmail.com>
2023-06-08 12:03:37 -0600

    Update useRegionalStatisticsOnADate.ts

--- src/services/ebird/hooks/endpoints/product/useRegionalStatisticsOnADate.ts
+++ src/services/ebird/hooks/endpoints/product/useRegionalStatisticsOnADate.ts
@@ -2,21 +2,28 @@ import useEbirdApi from '../../useEbirdApi';
 import yearMonthDayToUrlParams from '../../../helpers/yearMonthDayToUrlParams';
 import { type UrlParam } from '../../../types/EbirdApiParams';
 
-export default async function useRegionalStatisticsOnADate(
-  regionCode: string,
-  year: number,
-  month: number,
-  day: number
-) {
-  const urlParams: UrlParam[] = [
-    {
-      name: 'regionCode',
-      value: regionCode,
-    },
-    ...yearMonthDayToUrlParams(year, month, day),
-  ];
+export default function useRegionalStatisticsOnADate() {
+  const curriedMakeRequest = useEbirdApi();
 
-  return await useEbirdApi('product/stats/{{regionCode}}/{{y}}/{{m}}/{{d}}', {
-    urlParams,
-  });
+  return async function getRegionalStatisticsOnADate(
+    regionCode: string,
+    year: number,
+    month: number,
+    day: number
+  ) {
+    const urlParams: UrlParam[] = [
+      {
+        name: 'regionCode',
+        value: regionCode,
+      },
+      ...yearMonthDayToUrlParams(year, month, day),
+    ];
+
+    return await curriedMakeRequest(
+      'product/stats/{{regionCode}}/{{y}}/{{m}}/{{d}}',
+      {
+        urlParams,
+      }
+    );
+  };
 }
