f26ae33086a5f18c783684dc3723854aede33ebb
Christopher <ChrFimbel@gmail.com>
2023-06-08 12:15:44 -0600

    Update useHistoricObservationsOnADate.ts

--- src/services/ebird/hooks/endpoints/data/obs/useHistoricObservationsOnADate.ts
+++ src/services/ebird/hooks/endpoints/data/obs/useHistoricObservationsOnADate.ts
@@ -10,76 +10,80 @@ import type {
 
 export type EbirdHistoricalObservationRank = 'mrec' | 'create';
 
-export default async function useHistoricObservationsOnADate(
-  regionCode: string,
-  year: number,
-  month: number,
-  day: number,
-  category?: EbirdTaxonomyCategory,
-  detailLevel: EbirdObservationDetailLevel = 'simple',
-  hotspot = false,
-  includeProvisional = false,
-  maxResults?: number,
-  rank: EbirdHistoricalObservationRank = 'mrec',
-  locations?: string[],
-  speciesCommonNameLocale = 'en'
-) {
-  const urlParams: UrlParam[] = [
-    {
-      name: 'regionCode',
-      value: regionCode,
-    },
-    ...yearMonthDayToUrlParams(year, month, day),
-  ];
+export default function useHistoricObservationsOnADate() {
+  const curriedMakeRequest = useEbirdApi();
 
-  const categoryQueryParam: BaseQueryParam<EbirdTaxonomyCategory> = {
-    name: 'cat',
-    value: category,
+  return async function getHistoricObservationsOnADate(
+    regionCode: string,
+    year: number,
+    month: number,
+    day: number,
+    category?: EbirdTaxonomyCategory,
+    detailLevel: EbirdObservationDetailLevel = 'simple',
+    hotspot = false,
+    includeProvisional = false,
+    maxResults?: number,
+    rank: EbirdHistoricalObservationRank = 'mrec',
+    locations?: string[],
+    speciesCommonNameLocale = 'en'
+  ) {
+    const urlParams: UrlParam[] = [
+      {
+        name: 'regionCode',
+        value: regionCode,
+      },
+      ...yearMonthDayToUrlParams(year, month, day),
+    ];
+
+    const categoryQueryParam: BaseQueryParam<EbirdTaxonomyCategory> = {
+      name: 'cat',
+      value: category,
+    };
+
+    const detailLevelQueryParam: BaseQueryParam<EbirdObservationDetailLevel> = {
+      defaultValue: 'simple',
+      name: 'detail',
+      value: detailLevel,
+    };
+
+    const rankQueryParam: BaseQueryParam<EbirdHistoricalObservationRank> = {
+      defaultValue: 'mrec',
+      name: 'rank',
+      value: rank,
+    };
+
+    const queryParams: QueryParam[] = [
+      categoryQueryParam,
+      detailLevelQueryParam,
+      {
+        defaultValue: false,
+        name: 'hotspot',
+        value: hotspot,
+      },
+      {
+        defaultValue: false,
+        name: 'includeProvisional',
+        value: includeProvisional,
+      },
+      {
+        name: 'maxResults',
+        value: maxResults,
+      },
+      rankQueryParam,
+      {
+        name: 'r',
+        value: locations,
+      },
+      {
+        defaultValue: 'en',
+        name: 'sppLocale',
+        value: speciesCommonNameLocale,
+      },
+    ];
+
+    return await curriedMakeRequest(
+      'data/obs/{{regionCode}}/historic/{{y}}/{{m}}/{{d}}',
+      { urlParams, queryParams }
+    );
   };
-
-  const detailLevelQueryParam: BaseQueryParam<EbirdObservationDetailLevel> = {
-    defaultValue: 'simple',
-    name: 'detail',
-    value: detailLevel,
-  };
-
-  const rankQueryParam: BaseQueryParam<EbirdHistoricalObservationRank> = {
-    defaultValue: 'mrec',
-    name: 'rank',
-    value: rank,
-  };
-
-  const queryParams: QueryParam[] = [
-    categoryQueryParam,
-    detailLevelQueryParam,
-    {
-      defaultValue: false,
-      name: 'hotspot',
-      value: hotspot,
-    },
-    {
-      defaultValue: false,
-      name: 'includeProvisional',
-      value: includeProvisional,
-    },
-    {
-      name: 'maxResults',
-      value: maxResults,
-    },
-    rankQueryParam,
-    {
-      name: 'r',
-      value: locations,
-    },
-    {
-      defaultValue: 'en',
-      name: 'sppLocale',
-      value: speciesCommonNameLocale,
-    },
-  ];
-
-  return await useEbirdApi(
-    'data/obs/{{regionCode}}/historic/{{y}}/{{m}}/{{d}}',
-    { urlParams, queryParams }
-  );
 }
