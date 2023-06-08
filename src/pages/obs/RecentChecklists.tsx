e99e8155c5ce1bb50407c10c7594a54ad370a268
Christopher <ChrFimbel@gmail.com>
2023-06-08 14:30:19 -0600

    Update RecentChecklists.tsx

--- src/pages/obs/RecentChecklists.tsx
+++ src/pages/obs/RecentChecklists.tsx
@@ -3,10 +3,10 @@ import { useState } from 'react';
 import BasePageTableEbirdChecklist from '../../components/BasePageTableEbirdChecklist';
 import EbirdRegionCodeInput from '../../components/EbirdRegionCodeInput';
 import { NumberInput } from '../../components/NumberInput';
-import useEbirdApi from '../../services/ebird/useEbirdApi';
+import useRecentChecklistsFeed from '../../services/ebird/hooks/endpoints/data/obs/useRecentChecklistsFeed';
 
 export default function RecentChecklists() {
-  const { getRecentChecklists } = useEbirdApi();
+  const getRecentChecklists = useRecentChecklistsFeed();
 
   const [maxResults, setMaxResults] = useState(10);
   const [regionCode, setRegionCode] = useState('');
