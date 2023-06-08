2a88c3225483acd4d22087f698b98c932bef4251
Christopher <ChrFimbel@gmail.com>
2023-06-08 12:11:14 -0600

    Update useRecentNearbyObservations.ts

--- src/services/ebird/hooks/endpoints/data/obs/useRecentNearbyObservations.ts
+++ src/services/ebird/hooks/endpoints/data/obs/useRecentNearbyObservations.ts
@@ -7,72 +7,80 @@ import type {
 
 export type EbirdRecentNearbyObservationsSortBy = 'date' | 'species';
 
-export default async function useRecentNearbyObservations(
-  latitude: number,
-  longitude: number,
-  back = 14,
-  category?: EbirdTaxonomyCategory,
-  distance = 25,
-  hotspot = false,
-  includeProvisional = false,
-  maxResults?: number,
-  sort: EbirdRecentNearbyObservationsSortBy = 'date',
-  speciesCommonNameLocale = 'en'
-) {
-  const categoryQueryParam: BaseQueryParam<EbirdTaxonomyCategory> = {
-    name: 'cat',
-    value: category,
+export default function useRecentNearbyObservations() {
+  const curriedMakeRequest = useEbirdApi();
+
+  return async function getRecentNearbyObservations(
+    latitude: number,
+    longitude: number,
+    back = 14,
+    category?: EbirdTaxonomyCategory,
+    distance = 25,
+    hotspot = false,
+    includeProvisional = false,
+    maxResults?: number,
+    sort: EbirdRecentNearbyObservationsSortBy = 'date',
+    speciesCommonNameLocale = 'en'
+  ) {
+    const categoryQueryParam: BaseQueryParam<EbirdTaxonomyCategory> = {
+      name: 'cat',
+      value: category,
+    };
+
+    const sortQueryParam: BaseQueryParam<EbirdRecentNearbyObservationsSortBy> =
+      {
+        defaultValue: 'date',
+        name: 'sort',
+        value: sort,
+      };
+
+    const queryParams: QueryParam[] = [
+      {
+        name: 'lat',
+        value: latitude,
+      },
+      {
+        name: 'lng',
+        value: longitude,
+      },
+      {
+        defaultValue: 14,
+        name: 'back',
+        value: back,
+      },
+      categoryQueryParam,
+      {
+        defaultValue: 25,
+        name: 'dist',
+        value: distance,
+      },
+      {
+        defaultValue: false,
+        name: 'hotspot',
+        value: hotspot,
+      },
+      {
+        defaultValue: false,
+        name: 'includeProvisional',
+        value: includeProvisional,
+      },
+      {
+        name: 'maxResults',
+        value: maxResults,
+      },
+      sortQueryParam,
+      {
+        defaultValue: 'en',
+        name: 'sppLocale',
+        value: speciesCommonNameLocale,
+      },
+    ];
+
+    return await curriedMakeRequest(
+      'data/obs/geo/recent?lat={{lat}}&lng={{lng}}',
+      {
+        queryParams,
+      }
+    );
   };
-
-  const sortQueryParam: BaseQueryParam<EbirdRecentNearbyObservationsSortBy> = {
-    defaultValue: 'date',
-    name: 'sort',
-    value: sort,
-  };
-
-  const queryParams: QueryParam[] = [
-    {
-      name: 'lat',
-      value: latitude,
-    },
-    {
-      name: 'lng',
-      value: longitude,
-    },
-    {
-      defaultValue: 14,
-      name: 'back',
-      value: back,
-    },
-    categoryQueryParam,
-    {
-      defaultValue: 25,
-      name: 'dist',
-      value: distance,
-    },
-    {
-      defaultValue: false,
-      name: 'hotspot',
-      value: hotspot,
-    },
-    {
-      defaultValue: false,
-      name: 'includeProvisional',
-      value: includeProvisional,
-    },
-    {
-      name: 'maxResults',
-      value: maxResults,
-    },
-    sortQueryParam,
-    {
-      defaultValue: 'en',
-      name: 'sppLocale',
-      value: speciesCommonNameLocale,
-    },
-  ];
-
-  return await useEbirdApi('data/obs/geo/recent?lat={{lat}}&lng={{lng}}', {
-    queryParams,
-  });
 }
