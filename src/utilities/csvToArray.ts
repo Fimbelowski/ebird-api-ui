ddcd4f7a83c5bbfe6be169e33144036abcb5a36f
Christopher <ChrFimbel@gmail.com>
2023-06-10 12:09:21 -0600

    Improve typing.

--- src/utilities/csvToArray.ts
+++ src/utilities/csvToArray.ts
@@ -1,6 +1,6 @@
 export default function csvToArray<T>(
   csv: string,
-  headers: string[],
+  headers: Array<keyof T>,
   ignoreFirstLine = false
 ): T[] {
   const rows = csv.split(/\n(?=.)/);
@@ -12,12 +12,12 @@ export default function csvToArray<T>(
   return rows.map((row) => {
     const values = row.split(/(?!\B"[^"]*),(?![^"]*"\B)/g);
 
-    const rowObject: Record<string, string> = {};
+    const rowObject: Partial<Record<keyof T, string>> = {};
 
     headers.forEach((header, index) => {
       rowObject[header] = values[index];
     });
 
-    return rowObject;
-  }) as T[];
+    return rowObject as T;
+  });
 }
