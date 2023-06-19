63ef020dcf882626b7b5069b6eb0a21d4ffde346
Christopher <ChrFimbel@gmail.com>
2023-06-19 13:06:53 -0600

    Stub out page, add state.

--- src/pages/ref/taxonomy/EbirdTaxonomy.tsx
+++ src/pages/ref/taxonomy/EbirdTaxonomy.tsx
@@ -1,3 +1,20 @@
+import { useState } from 'react';
+
+import { BasePage } from '../../../components/BasePage/BasePage';
+import type EbirdTaxonomyCategory from '../../../types/EbirdTaxonomyCategory';
+import type EbirdRecordFormat from '../../../types/EbirdRecordFormat';
+
 export default function EbirdTaxonomy() {
-  return <></>;
+  const [category, setCategory] = useState<EbirdTaxonomyCategory>('');
+  const [format, setFormat] = useState<EbirdRecordFormat>('csv');
+  const [locale, setLocale] = useState('en');
+  const [species, setSpecies] = useState('');
+  const [version, setVersion] = useState('');
+
+  return (
+    <BasePage
+      requiresApiKey
+      title="eBird Taxonomy"
+    />
+  );
 }
