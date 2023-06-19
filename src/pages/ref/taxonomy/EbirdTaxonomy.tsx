c795a42695cb6838aa96d0bfd728f338138c64bb
Christopher <ChrFimbel@gmail.com>
2023-06-19 13:08:11 -0600

    Setup request.

--- src/pages/ref/taxonomy/EbirdTaxonomy.tsx
+++ src/pages/ref/taxonomy/EbirdTaxonomy.tsx
@@ -3,16 +3,24 @@ import { useState } from 'react';
 import { BasePage } from '../../../components/BasePage/BasePage';
 import type EbirdTaxonomyCategory from '../../../types/EbirdTaxonomyCategory';
 import type EbirdRecordFormat from '../../../types/EbirdRecordFormat';
+import { useEbirdTaxonomy } from '../../../services/ebird/hooks/endpoints/ref/taxonomy/useEbirdTaxonomy';
 
 export default function EbirdTaxonomy() {
+  const getEbirdTaxonomy = useEbirdTaxonomy();
+
   const [category, setCategory] = useState<EbirdTaxonomyCategory>('');
   const [format, setFormat] = useState<EbirdRecordFormat>('csv');
   const [locale, setLocale] = useState('en');
   const [species, setSpecies] = useState('');
   const [version, setVersion] = useState('');
 
+  async function onSubmit() {
+    return await getEbirdTaxonomy(category, format, locale, species, version);
+  }
+
   return (
     <BasePage
+      onSubmit={onSubmit}
       requiresApiKey
       title="eBird Taxonomy"
     />
