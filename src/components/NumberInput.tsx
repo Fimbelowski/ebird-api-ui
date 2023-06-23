009e0520c1e4bbc47e4aaaae860a247d03bf0abe
Christopher <ChrFimbel@gmail.com>
2023-06-23 14:20:24 -0600

    Require value on NumberInput.

--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -3,7 +3,9 @@ import { BaseInput, type BaseInputProps } from './BaseInput';
 export type NumberInputProps = Omit<
   BaseInputProps,
   'maxLength' | 'minLength' | 'type'
->;
+> & {
+  value: string;
+};
 
 export function NumberInput(props: NumberInputProps) {
   return (
