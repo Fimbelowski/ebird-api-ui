ba2bf387c2e080b3f0273f1c9cb52034c1c8fdda
Christopher <ChrFimbel@gmail.com>
2023-06-24 15:24:26 -0600

    Setup state and request.

--- src/pages/obs/RecentNearbyNotableObservations.tsx
+++ src/pages/obs/RecentNearbyNotableObservations.tsx
@@ -1,3 +1,40 @@
+import { useState } from 'react';
+
+import BasePageTableEbirdObservation from '../../components/BasePageTableEbirdObservation';
+import useRecentNearbyNotableObservations from '../../services/ebird/hooks/endpoints/data/obs/useRecentNearbyNotableObservations';
+import type EbirdObservationDetailLevel from '../../types/EbirdObservationDetailLevel';
+
 export default function RecentNearbyNotableObservations() {
-  return <></>;
+  const getRecentNearbyNotableObservations =
+    useRecentNearbyNotableObservations();
+
+  const [back, setBack] = useState();
+  const [detailLevel, setDetailLevel] =
+    useState<EbirdObservationDetailLevel>('simple');
+  const [distance, setDistance] = useState('');
+  const [hotspot, setHotspot] = useState(false);
+  const [latitude, setLatitude] = useState('');
+  const [locale, setLocale] = useState('en');
+  const [longitude, setLongitude] = useState('');
+  const [maxResults, setMaxResults] = useState('');
+
+  async function onSubmit() {
+    return await getRecentNearbyNotableObservations(
+      latitude,
+      longitude,
+      back,
+      detailLevel,
+      distance,
+      hotspot,
+      maxResults,
+      locale
+    );
+  }
+
+  return (
+    <BasePageTableEbirdObservation
+      onSubmit={onSubmit}
+      title="Recent Nearby Notable Observations"
+    />
+  );
 }
