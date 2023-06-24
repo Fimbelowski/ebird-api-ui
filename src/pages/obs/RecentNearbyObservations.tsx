f2b101c7ddbc90dd66fa95945d4ec10a807412c8
Christopher <ChrFimbel@gmail.com>
2023-06-24 15:48:16 -0600

    Setup state and request.

--- src/pages/obs/RecentNearbyObservations.tsx
+++ src/pages/obs/RecentNearbyObservations.tsx
@@ -1,3 +1,46 @@
+import { useState } from 'react';
+
+import BasePageTableEbirdObservation from '../../components/BasePageTableEbirdObservation';
+import {
+  useRecentNearbyObservations,
+  type EbirdRecentNearbyObservationsSortBy,
+} from '../../services/ebird/hooks/endpoints/data/obs/useRecentNearbyObservations';
+import type EbirdTaxonomyCategory from '../../types/EbirdTaxonomyCategory';
+
 export default function RecentNearbyObservations() {
-  return <></>;
+  const getRecentNearbyObservations = useRecentNearbyObservations();
+
+  const [back, setBack] = useState('');
+  const [category, setCategory] = useState<EbirdTaxonomyCategory>('');
+  const [distance, setDistance] = useState('');
+  const [hotspot, setHotspot] = useState(false);
+  const [includeProvisional, setIncludeProvisional] = useState();
+  const [latitude, setLatitude] = useState('');
+  const [locale, setLocale] = useState('en');
+  const [longitude, setLongitude] = useState('');
+  const [maxResults, setMaxResults] = useState('');
+  const [sortBy, setSortBy] =
+    useState<EbirdRecentNearbyObservationsSortBy>('date');
+
+  async function onSubmit() {
+    return await getRecentNearbyObservations(
+      latitude,
+      longitude,
+      back,
+      category,
+      distance,
+      hotspot,
+      includeProvisional,
+      maxResults,
+      sortBy,
+      locale
+    );
+  }
+
+  return (
+    <BasePageTableEbirdObservation
+      onSubmit={onSubmit}
+      title="Recent Nearby Observations"
+    />
+  );
 }
