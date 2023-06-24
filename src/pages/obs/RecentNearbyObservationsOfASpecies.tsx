1a60308530265b3ef15f5dbb27eb956eca29e8c4
Christopher <ChrFimbel@gmail.com>
2023-06-24 16:14:01 -0600

    Setup form content.

--- src/pages/obs/RecentNearbyObservationsOfASpecies.tsx
+++ src/pages/obs/RecentNearbyObservationsOfASpecies.tsx
@@ -2,6 +2,14 @@ import { useState } from 'react';
 
 import BasePageTableEbirdObservation from '../../components/BasePageTableEbirdObservation';
 import useRecentNearbyObservationsOfASpecies from '../../services/ebird/hooks/endpoints/data/obs/useRecentNearbyObservationsOfASpecies';
+import EbirdSpeciesCodeInput from '../../components/EbirdSpeciesCodeInput';
+import LocationInputGroup from '../../components/LocationInputGroup/LocationInputGroup';
+import BackInput from '../../components/BaseInput/BackInput';
+import { NumberInput } from '../../components/NumberInput';
+import EbirdOnlyObsFromHotspotsInput from '../../components/EbirdOnlyObsFromHotspotsInput';
+import EbirdIncludeProvisionalInput from '../../components/EbirdIncludeProvisionalInput';
+import MaxResultsInput from '../../components/MaxResultsInput';
+import LocaleSelect from '../../components/LocaleSelect/LocaleSelect';
 
 export default function RecentNearbyObservationsOfASpecies() {
   const getRecentNearbyObservationsOfASpecies =
@@ -31,8 +39,55 @@ export default function RecentNearbyObservationsOfASpecies() {
     );
   }
 
+  const formContent = (
+    <>
+      <EbirdSpeciesCodeInput
+        onChange={setSpeciesCode}
+        required
+        value={speciesCode}
+      />
+      <LocationInputGroup
+        latitude={latitude}
+        longitude={longitude}
+        setLatitude={setLatitude}
+        setLongitude={setLongitude}
+      />
+      <BackInput
+        onChange={setBack}
+        placeholder="14"
+        value={back}
+      />
+      <NumberInput
+        id="distance"
+        label="Distance"
+        max={50}
+        min={0}
+        onChange={setDistance}
+        placeholder="25"
+        value={distance}
+      />
+      <EbirdOnlyObsFromHotspotsInput
+        onChange={setHotspot}
+        value={hotspot}
+      />
+      <EbirdIncludeProvisionalInput
+        onChange={setIncludeProvisional}
+        value={includeProvisional}
+      />
+      <MaxResultsInput
+        onChange={setMaxResults}
+        value={maxResults}
+      />
+      <LocaleSelect
+        onChange={setLocale}
+        value={locale}
+      />
+    </>
+  );
+
   return (
     <BasePageTableEbirdObservation
+      formContent={formContent}
       onSubmit={onSubmit}
       title="Recent Nearby Observations of a Species"
     />
