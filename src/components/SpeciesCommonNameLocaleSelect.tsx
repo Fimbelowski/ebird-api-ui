3cd415ddb5a2b8e62a7b6171b2b6389170985540
Christopher <ChrFimbel@gmail.com>
2023-07-02 14:15:28 -0600

    Update SpeciesCommonNameLocaleSelect.tsx

--- src/components/SpeciesCommonNameLocaleSelect.tsx
+++ src/components/SpeciesCommonNameLocaleSelect.tsx
@@ -1,12 +1,23 @@
 import { useContext } from 'react';
 
 import LocaleOptionsContext from '../context/LocaleOptionsContext';
-import type { SelectProps } from './Select/Select';
 import useTaxaLocaleCodes from '../services/ebird/hooks/endpoints/ref/taxonomy/useTaxaLocaleCodes';
 import type EbirdTaxaLocaleCode from '../types/EbirdTaxaLocaleCode';
-import AsyncResourceSelect from './AsyncResourceSelect/AsyncResourceSelect';
+import {
+  AsyncResourceSelect,
+  type AsyncResourceSelectProps,
+} from './AsyncResourceSelect/AsyncResourceSelect';
 
-type Props = Omit<SelectProps<string>, 'id' | 'label' | 'options'>;
+type Props = Omit<
+  AsyncResourceSelectProps<string>,
+  | 'hasQueried'
+  | 'id'
+  | 'label'
+  | 'onLoad'
+  | 'onLoadMoreClick'
+  | 'options'
+  | 'resourcePlural'
+>;
 
 export default function SpeciesCommonNameLocaleSelect(props: Props) {
   const { localeOptions, setLocaleOptions } = useContext(LocaleOptionsContext);
