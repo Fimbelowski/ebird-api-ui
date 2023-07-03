e991282e252e2552fbebdab8c3fb9276d11c2b4e
Christopher <ChrFimbel@gmail.com>
2023-07-03 17:27:47 -0600

    Update HotspotInfo.tsx

--- src/pages/ref/hotspot/HotspotInfo.tsx
+++ src/pages/ref/hotspot/HotspotInfo.tsx
@@ -1,17 +1,13 @@
 import { useState } from 'react';
 
-import { BasePage } from '../../../components/BasePage/BasePage';
-import Details from '../../../components/Details/Details';
+import { BasePageKeyValuePairsList } from '../../../components/BasePageKeyValuePairsList';
 import type EbirdLocation from '../../../types/EbirdLocation';
-import EbirdLocationTableDetailed from '../../../components/EbirdLocationTableDetailed';
-import EbirdLocationTableSimple from '../../../components/EbirdLocationTableSimple';
 import { TextInput } from '../../../components/TextInput';
 import useHotspotInfo from '../../../services/ebird/hooks/endpoints/ref/hotspot/useHotspotInfo';
 
 export default function HotspotInfo() {
   const getHotspotInfo = useHotspotInfo();
 
-  const [hotspot, setHotspot] = useState<EbirdLocation>();
   const [locationId, setLocationId] = useState('');
 
   async function onSubmit() {
@@ -29,28 +25,11 @@ export default function HotspotInfo() {
     />
   );
 
-  const resultsContent =
-    hotspot !== undefined ? (
-      <>
-        <Details summary="Detailed Table">
-          <EbirdLocationTableDetailed locations={[hotspot]} />
-        </Details>
-        <Details
-          open
-          summary="Simplified Table"
-        >
-          <EbirdLocationTableSimple locations={[hotspot]} />
-        </Details>
-      </>
-    ) : null;
-
   return (
-    <BasePage<EbirdLocation>
+    <BasePageKeyValuePairsList<EbirdLocation>
       description="Fetches information about a specified hotspot."
       formContent={formContent}
-      onLoad={setHotspot}
       onSubmit={onSubmit}
-      resultsContent={resultsContent}
       title="Hotspot Info"
     />
   );
