d6bf83c41d6822f5c235742e05aa19c07ceb36b6
Christopher <ChrFimbel@gmail.com>
2023-07-11 14:45:16 -0600

    Render Outlet directly.

--- src/routes/regions/root.tsx
+++ src/routes/regions/root.tsx
@@ -1,11 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
 import type ExtendedRouteObject from '../ExtendedRouteObject';
 import regionInfo from './regionInfo';
 import subregionList from './subregionList';
-import Root from '../../pages/ref/region/Root';
 
 const root: ExtendedRouteObject = {
   children: [regionInfo, subregionList],
-  element: <Root />,
+  element: <Outlet />,
   path: 'regions',
 };
 
