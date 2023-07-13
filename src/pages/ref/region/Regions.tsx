1dd12c58942d7e82febe583817c1539ab77cac68
Christopher <ChrFimbel@gmail.com>
2023-07-13 13:32:11 -0600

    Create Regions.tsx

---
+++ src/pages/ref/region/Regions.tsx
@@ -0,0 +1,28 @@
+import BaseIndexPage from '../../../components/BaseIndexPage/BaseIndexPage';
+import type PageLink from '../../../types/PageLink';
+
+export default function Regions() {
+  const pageLinks: PageLink[] = [
+    {
+      description:
+        'Get information on the name and geographical area covered by a region.',
+      path: 'region-info',
+      requiresApiKey: true,
+      title: 'Region Info',
+    },
+    {
+      description:
+        'Fetches the list of sub-regions within a specified country or region.',
+      path: 'sub-region-list',
+      requiresApiKey: true,
+      title: 'Sub-region List',
+    },
+  ];
+
+  return (
+    <BaseIndexPage
+      pageLinks={pageLinks}
+      title="Regions"
+    />
+  );
+}
