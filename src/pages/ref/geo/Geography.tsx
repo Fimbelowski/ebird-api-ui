da6130c743c193928fb6d9ac99cbbcae270c14b9
Christopher <ChrFimbel@gmail.com>
2023-07-13 13:05:51 -0600

    Update Geography.tsx

--- src/pages/ref/geo/Geography.tsx
+++ src/pages/ref/geo/Geography.tsx
@@ -1,3 +1,20 @@
+import BaseIndexPage from '../../../components/BaseIndexPage/BaseIndexPage';
+import type { PageLinkProps } from '../../../components/PageLink/PageLink';
+
 export default function Geography() {
-  return <></>;
+  const pageLinks: PageLinkProps[] = [
+    {
+      description:
+        'Fetches the list of countries or regions that share a border with the specified region.',
+      path: 'adjacent-regions',
+      title: 'Adjacent Regions',
+    },
+  ];
+
+  return (
+    <BaseIndexPage
+      pageLinks={pageLinks}
+      title="Geography"
+    />
+  );
 }
