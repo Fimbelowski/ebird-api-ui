8f3a7ecfd376f5e4d57191e4776937b387318ff8
Christopher <ChrFimbel@gmail.com>
2023-07-13 13:52:02 -0600

    Fix typo in routes.

--- src/routes.tsx
+++ src/routes.tsx
@@ -161,7 +161,7 @@ export default createRoutesFromChildren(
       />
       <Route
         element={<TaxonomyVersions />}
-        path="taxonomy-verions"
+        path="taxonomy-versions"
       />
     </Route>
   </Route>
