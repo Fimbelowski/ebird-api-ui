e490fd9acc6d36a81095ea039ccb1e25bcc3dc59
Christopher <ChrFimbel@gmail.com>
2023-07-13 14:05:27 -0600

    Add description prop to BaseIndexPage.

--- src/components/BaseIndexPage/BaseIndexPage.tsx
+++ src/components/BaseIndexPage/BaseIndexPage.tsx
@@ -2,11 +2,16 @@ import PageLink from '../PageLink/PageLink';
 import type PageLinkInterface from '../../types/PageLink';
 
 interface Props {
+  description: string;
   pageLinks: PageLinkInterface[];
   title: string;
 }
 
-export default function BaseIndexPage({ pageLinks, title }: Props) {
+export default function BaseIndexPage({
+  description,
+  pageLinks,
+  title,
+}: Props) {
   const listItems = pageLinks.map((pageLink) => (
     <PageLink
       {...pageLink}
@@ -17,6 +22,7 @@ export default function BaseIndexPage({ pageLinks, title }: Props) {
   return (
     <div className="base-index-page">
       <h2 className="base-index-page__title">{title}</h2>
+      <p className="base-index-page__description">{description}</p>
       <menu className="base-index-page__menu">{listItems}</menu>
     </div>
   );
