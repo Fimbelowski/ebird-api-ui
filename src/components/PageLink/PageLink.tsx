be1ee36b949845def0b2824e334df09653efb77e
Christopher <ChrFimbel@gmail.com>
2023-07-13 14:29:16 -0600

    Update PageLink.tsx

--- src/components/PageLink/PageLink.tsx
+++ src/components/PageLink/PageLink.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 
 import type PageLinkInterface from '../../types/PageLink';
+import Tooltip from '../Tooltip/Tooltip';
 
 export default function PageLink({
   description,
@@ -8,14 +9,23 @@ export default function PageLink({
   title,
   requiresApiKey = false,
 }: PageLinkInterface) {
+  function Icon() {
+    const icon = requiresApiKey ? '🔑' : '🔓';
+    const tooltipMessage = `This endpoint ${
+      requiresApiKey ? 'requires' : 'does not require'
+    } an API key.`;
+
+    return <Tooltip text={tooltipMessage}>{icon}</Tooltip>;
+  }
+
   return (
     <Link
       className="page-link"
       to={path}
     >
-      <h3 className="page-link__title">{`${
-        requiresApiKey ? '🔑' : '🔓'
-      } ${title}`}</h3>
+      <h3 className="page-link__title">
+        <Icon /> {title}
+      </h3>
       <p>{description}</p>
     </Link>
   );
