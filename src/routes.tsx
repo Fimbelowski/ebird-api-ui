901a4ef302296adcaf95bd2fbd7e24faefe19f54
Christopher <ChrFimbel@gmail.com>
2023-07-14 12:55:04 -0600

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import AdjacentRegions from './pages/ref/geo/AdjacentRegions';
 import BaseIndexPage from './components/BaseIndexPage/BaseIndexPage';
 import ChecklistFeedOnADate from './pages/product/ChecklistFeedOnADate';
 import EbirdTaxonomy from './pages/ref/taxonomy/EbirdTaxonomy';
+import Error from './pages/Error';
 import Feedback from './pages/Feedback';
 import geographyPages from './pages/ref/geo/pages';
 import HistoricObservationsOnADate from './pages/obs/HistoricObservationsOnADate';
@@ -43,189 +44,191 @@ export default createRoutesFromChildren(
     path={PATH.Root}
     element={<Root />}
   >
-    <Route
-      element={<Index />}
-      index
-    />
-    <Route
-      element={<Feedback />}
-      path={PATH.Feedback}
-    />
-    <Route path={PATH.Geography}>
+    <Route errorElement={<Error />}>
       <Route
-        element={
-          <BaseIndexPage
-            description="Find a country's or region's neighbors."
-            pages={geographyPages}
-            title="Geography"
-          />
-        }
+        element={<Index />}
         index
       />
       <Route
-        element={<AdjacentRegions />}
-        path={PATH.AdjacentRegions}
+        element={<Feedback />}
+        path={PATH.Feedback}
+      />
+      <Route path={PATH.Geography}>
+        <Route
+          element={
+            <BaseIndexPage
+              description="Find a country's or region's neighbors."
+              pages={geographyPages}
+              title="Geography"
+            />
+          }
+          index
+        />
+        <Route
+          element={<AdjacentRegions />}
+          path={PATH.AdjacentRegions}
+        />
+      </Route>
+      <Route path={PATH.Hotspots}>
+        <Route
+          element={
+            <BaseIndexPage
+              description="Find the hotspots for a given country or region or nearby hotspots."
+              pages={hotspotPages}
+              title="Hotspots"
+            />
+          }
+          index
+        />
+        <Route
+          element={<HotspotInfo />}
+          path={PATH.HotspotInfo}
+        />
+        <Route
+          element={<HotspotsInARegion />}
+          path={PATH.HotspotsInARegion}
+        />
+        <Route
+          element={<NearbyHotspots />}
+          path={PATH.NearbyHotspots}
+        />
+      </Route>
+      <Route path={PATH.Observations}>
+        <Route
+          element={
+            <BaseIndexPage
+              description="Fetch observations submitted to eBird in checklists. Either for a specific country, region or location, or for nearby locations."
+              pages={observationsPages}
+              title="Observations"
+            />
+          }
+          index
+        />
+        <Route
+          element={<HistoricObservationsOnADate />}
+          path={PATH.HistoricObservationsOnADate}
+        />
+        <Route
+          element={<NearestObservationOfASpecies />}
+          path={PATH.NearestObservationsOfASpecies}
+        />
+        <Route
+          element={<RecentChecklistsFeed />}
+          path={PATH.RecentChecklistsFeed}
+        />
+        <Route
+          element={<RecentNearbyNotableObservations />}
+          path={PATH.RecentNearbyNotableObservations}
+        />
+        <Route
+          element={<RecentNearbyObservations />}
+          path={PATH.RecentNearbyObservations}
+        />
+        <Route
+          element={<RecentNearbyObservationsOfASpecies />}
+          path={PATH.RecentNearbyObservationsOfASPecies}
+        />
+        <Route
+          element={<RecentNotableObservationsInARegion />}
+          path={PATH.RecentNotableObservationsInARegion}
+        />
+        <Route
+          element={<RecentObservationsInARegion />}
+          path={PATH.RecentObservationsInARegion}
+        />
+        <Route
+          element={<RecentObservationsOfASpeciesInARegion />}
+          path={PATH.RecentObservationsOfASpeciesInARegion}
+        />
+      </Route>
+      <Route path={PATH.Product}>
+        <Route
+          element={
+            <BaseIndexPage
+              description="Product end-points make it easy to get the information shown in various pages on the eBird web site (Top 100 Contributors, checklist data, etc.)"
+              pages={productPages}
+              title="Product"
+            />
+          }
+          index
+        />
+        <Route
+          element={<ChecklistFeedOnADate />}
+          path={PATH.ChecklistFeedOnADate}
+        />
+        <Route
+          element={<RegionalStatisticsOnADate />}
+          path={PATH.RegionalStatisticsOnADate}
+        />
+        <Route
+          element={<SpeciesListForARegion />}
+          path={PATH.SpeciesListForARegion}
+        />
+        <Route
+          element={<Top100 />}
+          path={PATH.Top100}
+        />
+        <Route
+          element={<ViewChecklist />}
+          path={PATH.ViewChecklist}
+        />
+      </Route>
+      <Route path={PATH.Regions}>
+        <Route
+          element={
+            <BaseIndexPage
+              description="Get information about a specific region or get a list of all regions within a given region."
+              pages={regionPages}
+              title="Regions"
+            />
+          }
+          index
+        />
+        <Route
+          element={<RegionInfo />}
+          path={PATH.RegionInfo}
+        />
+        <Route
+          element={<SubregionList />}
+          path={PATH.SubregionList}
+        />
+      </Route>
+      <Route path={PATH.Taxonomy}>
+        <Route
+          element={
+            <BaseIndexPage
+              description="Get taxonomic information about species as well as information about the taxonomy itself."
+              pages={taxonomyPages}
+              title="Taxonomy"
+            />
+          }
+          index
+        />
+        <Route
+          element={<EbirdTaxonomy />}
+          path={PATH.EbirdTaxonomy}
+        />
+        <Route
+          element={<TaxaLocaleCodes />}
+          path={PATH.TaxaLocaleCodes}
+        />
+        <Route
+          element={<TaxonomicForms />}
+          path={PATH.TaxonomicForms}
+        />
+        <Route
+          element={<TaxonomicGroups />}
+          path={PATH.TaxonomicGroups}
+        />
+        <Route
+          element={<TaxonomyVersions />}
+          path={PATH.TaxonomyVersions}
+        />
+      </Route>
+      <Route
+        element={<NotFound />}
+        path={PATH.NotFound}
       />
     </Route>
-    <Route path={PATH.Hotspots}>
-      <Route
-        element={
-          <BaseIndexPage
-            description="Find the hotspots for a given country or region or nearby hotspots."
-            pages={hotspotPages}
-            title="Hotspots"
-          />
-        }
-        index
-      />
-      <Route
-        element={<HotspotInfo />}
-        path={PATH.HotspotInfo}
-      />
-      <Route
-        element={<HotspotsInARegion />}
-        path={PATH.HotspotsInARegion}
-      />
-      <Route
-        element={<NearbyHotspots />}
-        path={PATH.NearbyHotspots}
-      />
-    </Route>
-    <Route path={PATH.Observations}>
-      <Route
-        element={
-          <BaseIndexPage
-            description="Fetch observations submitted to eBird in checklists. Either for a specific country, region or location, or for nearby locations."
-            pages={observationsPages}
-            title="Observations"
-          />
-        }
-        index
-      />
-      <Route
-        element={<HistoricObservationsOnADate />}
-        path={PATH.HistoricObservationsOnADate}
-      />
-      <Route
-        element={<NearestObservationOfASpecies />}
-        path={PATH.NearestObservationsOfASpecies}
-      />
-      <Route
-        element={<RecentChecklistsFeed />}
-        path={PATH.RecentChecklistsFeed}
-      />
-      <Route
-        element={<RecentNearbyNotableObservations />}
-        path={PATH.RecentNearbyNotableObservations}
-      />
-      <Route
-        element={<RecentNearbyObservations />}
-        path={PATH.RecentNearbyObservations}
-      />
-      <Route
-        element={<RecentNearbyObservationsOfASpecies />}
-        path={PATH.RecentNearbyObservationsOfASPecies}
-      />
-      <Route
-        element={<RecentNotableObservationsInARegion />}
-        path={PATH.RecentNotableObservationsInARegion}
-      />
-      <Route
-        element={<RecentObservationsInARegion />}
-        path={PATH.RecentObservationsInARegion}
-      />
-      <Route
-        element={<RecentObservationsOfASpeciesInARegion />}
-        path={PATH.RecentObservationsOfASpeciesInARegion}
-      />
-    </Route>
-    <Route path={PATH.Product}>
-      <Route
-        element={
-          <BaseIndexPage
-            description="Product end-points make it easy to get the information shown in various pages on the eBird web site (Top 100 Contributors, checklist data, etc.)"
-            pages={productPages}
-            title="Product"
-          />
-        }
-        index
-      />
-      <Route
-        element={<ChecklistFeedOnADate />}
-        path={PATH.ChecklistFeedOnADate}
-      />
-      <Route
-        element={<RegionalStatisticsOnADate />}
-        path={PATH.RegionalStatisticsOnADate}
-      />
-      <Route
-        element={<SpeciesListForARegion />}
-        path={PATH.SpeciesListForARegion}
-      />
-      <Route
-        element={<Top100 />}
-        path={PATH.Top100}
-      />
-      <Route
-        element={<ViewChecklist />}
-        path={PATH.ViewChecklist}
-      />
-    </Route>
-    <Route path={PATH.Regions}>
-      <Route
-        element={
-          <BaseIndexPage
-            description="Get information about a specific region or get a list of all regions within a given region."
-            pages={regionPages}
-            title="Regions"
-          />
-        }
-        index
-      />
-      <Route
-        element={<RegionInfo />}
-        path={PATH.RegionInfo}
-      />
-      <Route
-        element={<SubregionList />}
-        path={PATH.SubregionList}
-      />
-    </Route>
-    <Route path={PATH.Taxonomy}>
-      <Route
-        element={
-          <BaseIndexPage
-            description="Get taxonomic information about species as well as information about the taxonomy itself."
-            pages={taxonomyPages}
-            title="Taxonomy"
-          />
-        }
-        index
-      />
-      <Route
-        element={<EbirdTaxonomy />}
-        path={PATH.EbirdTaxonomy}
-      />
-      <Route
-        element={<TaxaLocaleCodes />}
-        path={PATH.TaxaLocaleCodes}
-      />
-      <Route
-        element={<TaxonomicForms />}
-        path={PATH.TaxonomicForms}
-      />
-      <Route
-        element={<TaxonomicGroups />}
-        path={PATH.TaxonomicGroups}
-      />
-      <Route
-        element={<TaxonomyVersions />}
-        path={PATH.TaxonomyVersions}
-      />
-    </Route>
-    <Route
-      element={<NotFound />}
-      path={PATH.NotFound}
-    />
   </Route>
 );
