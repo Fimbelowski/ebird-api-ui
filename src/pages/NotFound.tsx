f3687dfa20cb4ff4af2d9a9483cd8f696ee0d33f
Christopher <ChrFimbel@gmail.com>
2023-07-14 12:33:45 -0600

    Create NotFound.tsx

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+import BaseAuxiliaryPage from '../components/BaseAuxiliaryPage/BaseAuxiliaryPage';
+
+export default function NotFound() {
+  return (
+    <BaseAuxiliaryPage title="404 - Page Not Found">
+      <p>The page you were looking for could not be found.</p>
+    </BaseAuxiliaryPage>
+  );
+}
