8355ae3c1e8dc72ec4f187f673e7f0b844bcdfd7
Christopher <ChrFimbel@gmail.com>
2023-10-20 13:12:17 -0600

    Add sorts to BasePageTableEbirdRegion.

--- src/components/BasePageTableEbirdRegion.tsx
+++ src/components/BasePageTableEbirdRegion.tsx
@@ -3,6 +3,7 @@ import {
   type BasePageTableProps,
   type Tables,
 } from './BasePageTable';
+import { type SortConfig, SortDirection } from './Table/Table';
 
 export interface EbirdRegion {
   code: string;
@@ -12,16 +13,29 @@ export interface EbirdRegion {
 type Props = Omit<BasePageTableProps<EbirdRegion>, 'tables'>;
 
 export function BasePageTableEbirdRegion(props: Props) {
+  const sortConfig: Omit<SortConfig<EbirdRegion>, 'id'> = {
+    initialSortDirection: SortDirection.Ascending,
+    sort: (items: EbirdRegion[]) => items.toSorted(),
+  };
+
   const tables: Tables<EbirdRegion> = [
     {
       columns: [
         {
           callback: ({ name }) => name,
           label: 'Name',
+          sortConfig: {
+            ...sortConfig,
+            id: 'nameSort',
+          },
         },
         {
           callback: ({ code }) => code,
           label: 'Code',
+          sortConfig: {
+            ...sortConfig,
+            id: 'codeSort',
+          },
         },
       ],
       title: 'Results Table',
