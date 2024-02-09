6203f1cf4b1f6afb97076191f78c92e35d5adaf4
Christopher <ChrFimbel@gmail.com>
2024-02-08 19:34:07 -0700

    Update PAGE.ts

--- src/pages/ref/hotspot/PAGE.ts
+++ src/pages/ref/hotspot/PAGE.ts
@@ -4,16 +4,19 @@ const PAGE = {
   HotspotInfo: {
     description: 'Fetches information about a specified hotspot.',
     path: PATH.HotspotInfo,
+    requiresApiKey: true,
     title: 'Hotspot Info',
   },
   HotspotsInARegion: {
     description: 'Fetches hotspots within a specified country or region.',
     path: PATH.HotspotsInARegion,
+    requiresApiKey: true,
     title: 'Hotspots in a Region',
   },
   NearbyHotspots: {
     description: 'Fetches a list of hotspots near a given location.',
     path: PATH.NearbyHotspots,
+    requiresApiKey: true,
     title: 'Nearby Hotspots',
   },
 } as const;
